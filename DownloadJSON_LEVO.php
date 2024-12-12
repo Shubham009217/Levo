184c85c9af7533b55146a544111020af3b3815c2
Shubham009217 <shubhamdata20@gmail.com>
2024-12-12 20:01:06 +0530

    Update DownloadJSON_LEVO.php

--- DownloadJSON_LEVO.php
+++ DownloadJSON_LEVO.php
@@ -1,4 +1,4 @@
 {
   "version": "0.01",
-  "link": "https://raw.githubusercontent.com/dushyant210/vision/main/libAndroidX64.so"
+  "link": "https://raw.githubusercontent.com/Shubham009217/Levo/main/libGVoicePlugin.so"
 }
